b5cbad0a9f0a654c1acbda9d687660eb42493536
Seowoohyeon <66168708+Seowoohyeon@users.noreply.github.com>
2022-03-21 19:25:46 +0900

    Map을 이용한 배열 출력

--- src/array_01.java
+++ src/array_01.java
@@ -1,14 +1,52 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class array_01 {
+	
+	static int[] in = {1, 1, 2, 3, 4};
+	static char[] st = {'a', 'b', 'c', 'd', 'e'};
+	
+	public static void map_print_int() {
+		Map<Integer, Object> map1 = new HashMap<Integer, Object>();
+		map1.put(1, in[0]);
+		map1.put(2, in[1]);
+		map1.put(3, in[2]);
+		map1.put(4, in[3]);
+		map1.put(5, in[4]);
+		
+		Set<Integer> kset = map1.keySet();
+		System.out.println("정수형 배열 출력");
+		for(Integer k : kset) {
+			System.out.println(k + " : " + map1.get(k));
+		}
+		
+	}
+	
+	public static void map_print_char() {
+		Map<Integer, Object> map2 = new HashMap<Integer, Object>();
+		map2.put(1, st[0]);
+		map2.put(2, st[1]);
+		map2.put(3, st[2]);
+		map2.put(4, st[3]);
+		map2.put(5, st[4]);
+		
+		Set<Integer> kset = map2.keySet();
+		System.out.println("문자형 배열 출력");
+		for(Integer k : kset) {
+			System.out.println(k + " : " + map2.get(k));
+		}
+	}
 
 	public static void main(String[] args) {
 		
 		/*
-		  map을 사용하여 배열 출력 중복 되는 값 없이
+		  map을 사용하여 배열 출력 
 		 */
+		array_01 a1 = new array_01();
 		
-		int a = 0;
-		
+		a1.map_print_int();
+		a1.map_print_char();
 		
 	}
 	
--- src/array_02.java
+++ src/array_02.java
@@ -1,9 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class array_02 {
 	
 	public static void main(String[] args) {
 		
+		int[] a = {1, 1, 2, 3, 4};
 		
+		Map<Integer, Object> map = new HashMap<Integer, Object>();
+		map.put(1, a[0]);
+		map.put(2, a[1]);
+		map.put(3, a[2]);
+		map.put(4, a[3]);
+		map.put(5, a[4]);
+		
+		Set<Integer> kset = map.keySet();
+		for(Integer k : kset) {
+			System.out.println(k + " : " + map.get(k));
+		}
 		
 	}
 }
