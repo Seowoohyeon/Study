6c241ca5cac3bcedf9c7dcbaaa806777b778b3d1
Seowoohyeon <66168708+Seowoohyeon@users.noreply.github.com>
2022-03-23 00:30:30 +0900

    커커커커커밋

--- src/school/MachinelearningStudent.java
+++ src/school/MachinelearningStudent.java
@@ -16,7 +16,7 @@ public class MachinelearningStudent extends Student{
 	@Override
 	public void printGradeCard() {
 		System.out.println("¸Ó½Å·¯´× ¹İ");
-		System.out.printf("¶ô¹ø\tÀÌ¸§\t%s\t%s\t%s\t%s\tÃÑÁ¡\tÆò±Õ\n",
+		System.out.printf("ÇĞ¹ø\tÀÌ¸§\t%s\t%s\t%s\t%s\tÃÑÁ¡\tÆò±Õ\n",
 				sub[0],sub[1],sub[2],sub[3]);
 		System.out.printf("%X\t%s\t%d\t%d\t%d\t%d\t%d\t%.2f\n",
 				sno,name,kor,eng,math,pl, kor+eng+math+pl, (kor+eng+math+pl) / 4.0);
--- src/school/ProgrammingStudent.java
+++ src/school/ProgrammingStudent.java
@@ -15,7 +15,7 @@ public class ProgrammingStudent extends Student{
 	@Override
 	public void printGradeCard() {
 		System.out.println("프로그래밍 반");
-		System.out.printf("락번\t이름\t%s\t%s\t%s\t%s\t총점\t평균\n",
+		System.out.printf("학번\t이름\t%s\t%s\t%s\t%s\t총점\t평균\n",
 				sub[0],sub[1],sub[2],sub[3]);
 		System.out.printf("%X\t%s\t%d\t%d\t%d\t%d\t%d\t%.2f\n",
 				sno,name,kor,eng,math,pl, kor+eng+math+pl, (kor+eng+math+pl) / 4.0);
